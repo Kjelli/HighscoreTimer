169fc080f6634e8a6617054a79d41245bd2c421a
fabriece sumuni <sfabriece@hotmail.com>
2014-03-04 15:18:09 +0100

    clock working

--- HighscoreTimer/src/studentapplication/ClockPanel.java
+++ HighscoreTimer/src/studentapplication/ClockPanel.java
@@ -7,10 +7,24 @@ import java.awt.event.MouseListener;
 import java.sql.Time;
 
 public class ClockPanel extends JPanel implements MouseListener{
+    long t = 300000;
     public ClockPanel(){
         setPreferredSize(new Dimension(200, 200));
         setBackground(Color.BLUE);
         addMouseListener(this);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while(t >= 0){
+                    Thread.currentThread().sleep(1000);
+                        t -= 1000;
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     @Override
@@ -18,14 +32,16 @@ public class ClockPanel extends JPanel implements MouseListener{
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         //g2d.translate(getHeight()/2, getWidth()/2);
-        g2d.setFont(new Font("Arial", Font.BOLD, 20));
+        g2d.setFont(new Font("Arial", Font.BOLD, 25));
         g2d.setColor(Color.WHITE);
-        //System.out.println(getWidth()/2);
+        System.out.println(t);
         FontMetrics fm = g2d.getFontMetrics();
-        long t = System.currentTimeMillis();
+        //long t = System.currentTimeMillis();
 
-        int str = fm.stringWidth();
-        g2d.drawString((t/3600000) + " : " + (t/60000) + " : " + (t/1000), getWidth()/2 - str/2, getHeight()/2);
+        long min = (t/60000);
+        long sec = (t % 60000)/1000 ;
+        int str = fm.stringWidth(min + " : " + sec);
+        g2d.drawString(min + " : " + sec, getWidth()/2 - str/2, getHeight()/2);
         repaint();
     }
 
